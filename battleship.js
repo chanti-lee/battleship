9e59c522d4b5bf16bf6d5719de99358049810e7e
Chantelle Lee <clee17@dcs.bbk.ac.uk>
2019-08-01 12:44:02 +0100

    initial game logic written

--- battleship.js
+++ battleship.js
@@ -7,3 +7,29 @@ let hits = 0;
 let guesses = 0;
 
 let isSunk = false;
+
+while (isSunk == false) {
+	guess = prompt("Ready, aim, fire! (enter a number from 0-6 :");
+	if (guess < 0 || guess > 6) {
+		alert("Please enter a valid cell number!");
+	} else {
+		guesses += 1;
+		if (guess == location1 || guess == location2 || guess == location3) {
+			hits += 1;
+			alert("HIT!");
+			if (hits == 3) {
+				isSunk = true;
+				alert("You sank my battleship!");
+			}
+		}
+		else {
+			alert("MISS");
+		}
+
+		}
+	}
+
+var stats = "You took " + guesses + " guesses to sink the battleship, which means your shooting accuracy was "
++ (3/guesses);
+
+alert(stats);
