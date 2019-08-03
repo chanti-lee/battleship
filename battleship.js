882371054f8a91b42cd233168bfde6618daa1806
Chantelle Lee <clee17@dcs.bbk.ac.uk>
2019-08-03 20:12:58 +0100

    added methods to model object

--- battleship.js
+++ battleship.js
@@ -14,11 +14,36 @@ var view = {
 	}
 };
 
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
+var model = {
+	boardSize: 7,
+	numShips: 3,
+	shipLength: 3,
+	shipsSunk: 0,
 
-view.displayMessage("Testing message");
+	ships [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+		{ locations: ["24", "34", "44"], hits: ["", "", ""] },
+		{ locations: ["10", "11", "12"], hits: ["", "", ""]}],
+
+	fire: function(guess) {
+		for (var = 0; i < this.numShips; i++) {
+			var ship = this.ships[i];
+			var index = ship.locations.indexOf(guess);
+			if (index >= 0) {
+				ship.hits[index] = "hit";
+				if (this.isSunk(ship)) {
+					this.shipsSunk++;
+				}
+				return true;
+			}
+		}
+		return false;
+	},
+	isSunk: function(ship) {
+		for (var i = 0; i < this.shipLength; i++) {
+			if (ship.hits[i] !== "hit") {
+				return false;
+			}
+		}
+		return true;
+	}
+};
