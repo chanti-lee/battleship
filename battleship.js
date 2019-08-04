8e97814fff6ac6f8622bcffe051194f9144c44f5
Chantelle Lee <clee17@dcs.bbk.ac.uk>
2019-08-04 16:04:20 +0100

    testing

--- battleship.js
+++ battleship.js
@@ -20,12 +20,12 @@ var model = {
 	shipLength: 3,
 	shipsSunk: 0,
 
-	ships [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+	ships: [ { locations: ["06", "16", "26"], hits: ["", "", ""] },
 		{ locations: ["24", "34", "44"], hits: ["", "", ""] },
 		{ locations: ["10", "11", "12"], hits: ["", "", ""]}],
 
 	fire: function(guess) {
-		for (var = 0; i < this.numShips; i++) {
+		for (var i = 0; i < this.numShips; i++) {
 			var ship = this.ships[i];
 			var index = ship.locations.indexOf(guess);
 			if (index >= 0) {
@@ -52,3 +52,9 @@ var model = {
 		return true;
 	}
 };
+
+model.fire("53");
+
+model.fire("06");
+model.fire("16");
+model.fire("26");
